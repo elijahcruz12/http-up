5ffc0aa6e63e9b8d3c9216e0922bc369caf69a81
Elijah Cruz <ecwebservices@gmail.com>
2021-06-16 21:42:35 -0500

    Updated main to have more capabilities.

--- main.go
+++ main.go
@@ -3,19 +3,58 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
-	fmt.Println("Testing your internet connection")
 
 	resp, err := http.Get("http://example.com")
 
+	var first string
+
 	_ = resp
+
+	if os.Args != nil && len(os.Args) > 1 {
+		first = os.Args[1]
+	} else {
+		first = "unknown"
+	}
+
+	if first == "unknown" {
+		getLongAnswer(err)
+	} else if first == "long" {
+		getLongAnswer(err)
+	} else if first == "short" {
+		getShortAnswer(err)
+	} else {
+		fmt.Println("Option not defined")
+	}
+
+	getLongAnswer(err)
+}
+
+func getLongAnswer(err interface{}) {
+	fmt.Println("Testing your internet connection")
 
 	if err != nil {
 		fmt.Println("Internet is down")
+
+		os.Exit(1)
 	} else {
 		fmt.Println("Internet is working")
 
+		os.Exit(0)
+	}
+}
+
+func getShortAnswer(err interface{}) {
+	if err != nil {
+		fmt.Println("down")
+
+		os.Exit(1)
+	} else {
+		fmt.Println("up")
+
+		os.Exit(0)
 	}
 }
