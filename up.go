ed761d1b6195c8c89789d7f2262841d602b5c33d
Elijah Cruz <ecwebservices@gmail.com>
2021-06-24 16:28:56 -0500

    Official all works completely.

--- up.go
+++ up.go
@@ -4,11 +4,16 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
 
-	resp, err := http.Get("http://example.com")
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	resp, err := client.Get("http://example.com")
 
 	var first string
 
@@ -26,14 +31,32 @@ func main() {
 		getLongAnswer(err)
 	} else if first == "short" {
 		getShortAnswer(err)
+	} else if first == "help" {
+		helpScreen()
+	} else if first == "--help" {
+		helpScreen()
 	} else {
 		fmt.Println("Option not defined")
 	}
+}
 
-	getLongAnswer(err)
+func helpScreen() {
+	fmt.Println("Up (http-up) version 1.1.0")
+	fmt.Println("By Elijah Cruz")
+	fmt.Println()
+	fmt.Println("Allows you to check the status of your internet")
+	fmt.Println()
+	fmt.Printf("")
+	fmt.Println("Options:")
+	fmt.Println(" - <none>: Equivalent of long")
+	fmt.Println(" - long: Displays a longer more human readable version of your internet status.")
+	fmt.Println(" - short: Displays either \"up\" or \"down\" depending on your internet is up or down.")
+	fmt.Println(" - help: This help message")
 }
 
 func getLongAnswer(err interface{}) {
+
+	fmt.Println("Getting you internet status")
 
 	if err != nil {
 		fmt.Println("Internet is down")
