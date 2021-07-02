adc0598cc95c8bc00a411b38fa7b0c0890847af1
Elijah Cruz <ecwebservices@gmail.com>
2021-07-01 21:21:41 -0500

    Modified Docker build to give the container a name

--- docker-build.sh
+++ docker-build.sh
@@ -1,1 +1,1 @@
-docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:latest ./build.sh
+docker run --name="tasker_builder" --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:latest ./build.sh
